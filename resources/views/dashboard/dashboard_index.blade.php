46142122ee4358d0ceaeeaded386eb46b75da1ab
Matheus Mota <matheus.mat1996@gmail.com>
2024-02-22 08:47:25 -0300

    TechSupport v1.0

--- resources/views/dashboard/dashboard_index.blade.php
+++ resources/views/dashboard/dashboard_index.blade.php
@@ -1,19 +1,19 @@
-<x-pages.index>    
+<x-pages.index>
 
     <!-- Slot Header -->
     @slot('header')
         <x-header title="Dashboards"/>
     @endslot
-    
+
     @slot('body')
-        <x-conteiner> 
+        <x-conteiner>
             <div class="row">
                 @foreach ($db as $item)
-                    <div class="col-xl-2 col-md-4 col-6">                             
+                    <div class="col-xl-2 col-md-4 col-6">
                         @include('dashboard.partials.dashboard_card')
-                    </div>                                    
-                @endforeach  
-            </div>       
+                    </div>
+                @endforeach
+            </div>
         </x-conteiner>
     @endslot
 
