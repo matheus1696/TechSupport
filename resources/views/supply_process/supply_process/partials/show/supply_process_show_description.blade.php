441c9e133329f17302b9e2eca68a153adfdce8f4
Matheus Mota <matheus.mat1996@gmail.com>
2024-03-05 16:08:33 -0300

    TechSupport v1.0

--- resources/views/supply_process/supply_process/partials/show/supply_process_show_description.blade.php
+++ resources/views/supply_process/supply_process/partials/show/supply_process_show_description.blade.php
@@ -4,18 +4,8 @@
     <p><strong>Nº do Contrato: </strong>{{$db->code_contract ?? ""}}</p>
     <p><strong>Nº do Regristro de Preço: </strong>{{$db->code_contract ?? ""}}</p>
     <p><strong>Titulo: </strong>{{$db->title ?? ""}}</p>
-    <p class="col-lg">
-        <strong>Data Inicio: </strong>{{$db->start_date == NULL ? "" : date('d/m/Y',strtotime($db->start_date))}}
-    </p>
-    <p class="col-lg">
-        <strong>Data de Venc.: </strong>{{$db->due_date == NULL ? "" : date('d/m/Y',strtotime($db->due_date))}}
-    </p>
-    <p class="col-lg"><strong>Tempo de Vigência:</strong>
-        {{$db->validity}}
-        @if($db->validity) Meses @endif
-    </p>
-    <div class="mb-3 col-lg-12">
-        <strong>Objetivo: </strong>
-        {!! $db->description ?? "" !!}
-    </div>
+    <p><strong>Data Inicio: </strong> {{$db->start_date == NULL ? "" : date('d/m/Y',strtotime($db->start_date))}}</p>
+    <p><strong>Data de Venc.: </strong> {{$db->due_date == NULL ? "" : date('d/m/Y',strtotime($db->due_date))}}</p>
+    <p><strong>Tempo de Vigência: </strong> {{$db->validity}} @if($db->validity) Meses @endif </p>
+    <div><strong>Objetivo: </strong> {!! $db->description ?? "" !!} </div>
 </div>
